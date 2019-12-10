2ee9353154bd4cc067bbd683f6a1fb9247a4e8f5
Iulian Onofrei <iulian.onofrei@yahoo.com>
2019-12-11 00:19:20 +0200

    Added empty app

---
+++ EvaluateForXcode/AppDelegate.swift
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.swift
+//  EvaluateForXcode
+//
+//  Created by Iulian Onofrei on 11/12/2019.
+//  Copyright © 2019 Iulian Onofrei. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+	func applicationDidFinishLaunching(_ aNotification: Notification) {
+		// Insert code here to initialize your application
+	}
+
+	func applicationWillTerminate(_ aNotification: Notification) {
+		// Insert code here to tear down your application
+	}
+
+}
