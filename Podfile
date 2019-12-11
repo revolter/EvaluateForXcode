c9e385f74d2e4a99d068109af8679906dbf1459d
Iulian Onofrei <iulian.onofrei@yahoo.com>
2019-12-11 02:14:18 +0200

    Initialized CocoaPods

---
+++ Podfile
@@ -0,0 +1,7 @@
+platform :macos, '10.15'
+
+target 'EvaluateForXcode' do
+end
+
+target 'SourceEditorExtension' do
+end
---
+++ Pods/Target Support Files/Pods-SourceEditorExtension/Pods-SourceEditorExtension-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_SourceEditorExtension : NSObject
+@end
+@implementation PodsDummy_Pods_SourceEditorExtension
+@end
