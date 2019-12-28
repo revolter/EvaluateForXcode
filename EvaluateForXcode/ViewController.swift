79fc4d128c3bcfe369aef5514da04b2437e33352
Iulian Onofrei <iulian.onofrei@yahoo.com>
2019-12-28 11:28:34 +0200

    Reverted "Removed unused window from the app"
    (reverted from commit e24530da45a5c1a2c4957c347605519e1d71b516)

---
+++ EvaluateForXcode/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  EvaluateForXcode
+//
+//  Created by Iulian Onofrei on 11/12/2019.
+//  Copyright © 2019 Iulian Onofrei. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+
+		// Do any additional setup after loading the view.
+	}
+
+	override var representedObject: Any? {
+		didSet {
+		// Update the view, if already loaded.
+		}
+	}
+
+}
