895e91389cc93f0160c2e1db5f808cae3c276d56
Iulian Onofrei <iulian.onofrei@yahoo.com>
2019-12-28 23:23:35 +0200

    Refactor: Removed superfluous code

--- EvaluateForXcode/AppDelegate.swift
+++ EvaluateForXcode/AppDelegate.swift
@@ -9,14 +9,4 @@
 import Cocoa
 
 @NSApplicationMain
-class AppDelegate: NSObject, NSApplicationDelegate {
-
-	func applicationDidFinishLaunching(_ aNotification: Notification) {
-		NSApp.helpMenu?.removeAllItems()
-	}
-
-	func applicationWillTerminate(_ aNotification: Notification) {
-		// Insert code here to tear down your application
-	}
-
-}
+class AppDelegate: NSObject, NSApplicationDelegate {}
